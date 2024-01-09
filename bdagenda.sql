4e637fc458d714ef380c02eeb6fced09bca9b0f0
ralvac <155853610+ralvac@users.noreply.github.com>
2024-01-08 23:20:11 -0500

    Update bdagenda.sql

--- bdagenda.sql
+++ bdagenda.sql
@@ -29,25 +29,25 @@ ADD COLUMN distrito_id INT,
 ADD CONSTRAINT agenda_distrito_fk FOREIGN KEY (distrito_id) REFERENCES distrito(id);
 
 INSERT INTO agenda VALUES ('1', 'carlos', 'serna','av. palmeras 145',
-'043524875', '942587568','carlos@hotmail.com', 'carlos serna', 1);
+'043524875', '942587568','carlos@hotmail.com', 'carlos serna');
 INSERT INTO agenda VALUES ('2', 'carmen', 'figueroa','av. alamos f-5',
-'043154784', '943124578','carmen@hotmail.com', 'carmen figeroa', 2);
+'043154784', '943124578','carmen@hotmail.com', 'carmen figeroa');
 INSERT INTO agenda VALUES ('3', 'juan', 'perez','av.pacaes',
-'043758694', '943582674','juan@hotmail.com', 'juan perez', 3);
+'043758694', '943582674','juan@hotmail.com', 'juan perez');
 INSERT INTO agenda VALUES ('4', 'jean', 'ramirez','av.pardo 580',
-'043587469', '941748596','jean@hotmail.com', 'jean ramirez', 1);
+'043587469', '941748596','jean@hotmail.com', 'jean ramirez');
 INSERT INTO agenda VALUES ('5', 'melisa', 'cavero','av.huaraz 220',
-'043152648', '942121415','melisa@hotmail.com', 'melisa cavero', 2);
+'043152648', '942121415','melisa@hotmail.com', 'melisa cavero');
 INSERT INTO agenda VALUES ('6', 'klaus', 'arteaga','av.lima 145',
-'048757686', '943332524','klaus@hotmail.com', 'klaus arteaga', 3);
+'048757686', '943332524','klaus@hotmail.com', 'klaus arteaga');
 INSERT INTO agenda VALUES ('7', 'octavio', 'baldarrago','av.galvez 874',
-'043584736', '941747485','octavio585@hotmail.com', 'octavio baldarrago', 1);
+'043584736', '941747485','octavio585@hotmail.com', 'octavio baldarrago');
 INSERT INTO agenda VALUES ('8', 'angela', 'suarez','av.pardo 1894',
-'043252526', '941741212','angela_humilde@hotmail.com', 'angela suarez', 2);
+'043252526', '941741212','angela_humilde@hotmail.com', 'angela suarez');
 INSERT INTO agenda VALUES ('9', 'antonhy', 'orbegozo','av.palmeras 456',
-'043151919', '942321212','antonhyelgozo@hotmail.com', 'antonhy orbegozo', 3);
+'043151919', '942321212','antonhyelgozo@hotmail.com', 'antonhy orbegozo');
 INSERT INTO agenda VALUES ('10', 'giancarlo', 'sarrin','av.huaraz 123',
-'043411742', '9744105472','giancarlosarrines@hotmail.com', 'giancarlo sarrin', 1);
+'043411742', '9744105472','giancarlosarrines@hotmail.com', 'giancarlo sarrin');
 COMMIT;
 
 ALTER TABLE agenda
